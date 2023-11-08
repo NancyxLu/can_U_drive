21dc417a91aee8665f482085f2e9ea582d404448
Nancy Lu <nancy@cloudsoft.com.tw>
2023-11-08 18:50:21 +0800

    UPDATE in list

--- driving.py
+++ driving.py
@@ -3,16 +3,16 @@
 #在台灣 滿18可以考駕照
 #在美國 滿16可以考駕照
 
-place = input ('請問你住在哪個國家?')
-age = input ('請問你今年幾歲?')
+place = input ('請問你是台灣人還是美國人?  請輸入:')
+age = input ('請問你今年幾歲?  請輸入:')
 age = float (age)
 
-if place == '台灣':
+if place in ['台灣', '台', 'taiwan', '台灣人']:
 	if age >= 18 :
 		print ('恭喜你已滿', age, ',考到駕照就可以開車了!')
 	else :
 		print ('抱歉', age, '歲,還不能開車喔')
-elif  place == '美國' :
+elif  place in ['美國', '美', '美國人', 'USA']:
 	if age >= 16 :
 		print ('恭喜你已滿', age, ',考到駕照就可以開車了!')
 	else :
